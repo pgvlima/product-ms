e511a1bc356155062c793f629d16c37d0c9bd63c
Paulo Guilherme Veras Lima <A0090799@redecorp.br>
2021-09-24 11:10:34 -0300

    Commit projeto

--- catalogo/src/main/java/br/com/curso/product/domain/Product.java
+++ catalogo/src/main/java/br/com/curso/product/domain/Product.java
@@ -4,8 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive; 
 
 
 
@@ -14,9 +14,9 @@ public class Product {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String id;
-	@NotBlank(message = "Campo name não pode ser nulo")
+	@NotNull(message = "Campo name não pode ser nulo")
 	private String name;
-	@NotBlank (message = "Campo description não pode ser nulo")
+	@NotNull (message = "Campo description não pode ser nulo")
 	private String description;
 	@Positive (message = "Campo price não pode ser negativo")
 	private double price; 
